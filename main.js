f75c039ae9a853f9728b3c9b9b061c136b54d82d
sharveshBarath <81868895+sharveshBarath@users.noreply.github.com>
2022-04-09 12:45:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+difference=0;
+leftWristX = 0;
+rightWristX = 0;
 function setup(){
     canvas = createCanvas(550,550);
     canvas.position(560,150)
@@ -18,11 +21,18 @@ function setup(){
     if(results.length > 0){
     
         console.log(results)
+        leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor( leftWristX - rightWristX );
+
     }
     }
     function draw(){
-    background("#969A97")
-    
+        background('#6C91C2');
+    document.getElementById('font_size').innerHTML = "font size of the text = " + difference + "  px";
+    textSize(difference);
+    fill('#FFE787');
+text("Sharvesh", 50,400);
     
     
     
